4e42b521a3b93470ef38505f52201406ab8ab9d5
Hadrien G <knights_of_ni@gmx.com>
2020-04-19 12:05:46 +0200

    Another round of review, now with a full plan

--- src/lib.rs
+++ src/lib.rs
@@ -1,24 +1,73 @@
-//! This is basically the num_traits API, reimplemented using free functions.
+//! This is basically the num_traits API, re-exported as free functions.
 //!
-//! So, you're trying to do scientific computing in Rust, and you can't get
-//! used to mathematical functions like sin() or cos() being postfix methods?
-//! This crate may be for you!
+//! # Introduction
+//!
+//! If you're trying to do scientific computing in Rust, and you can't get used
+//! to mathematical functions like sin() or cos() being postfix methods, this
+//! crate may be for you!
 //!
 //! It re-export all useful free functions from `num_traits` in a single place
 //! and provides free function versions of the trait methods of the num traits,
 //! so that you can easily do things like `sin(x) + 3*ln(y)`.
 //!
-//! Each trait's methods are exposed as an identically names module of free
-//! functions in this crate, and it only takes one use clause to go from there
-//! to using the above syntax in your math expressions.
+//! Each trait's methods are exposed as an module of free functions bearing the
+//! trait's name, and it only takes one use clause to go from there to using the
+//! above syntax in your math expressions.
 //!
 //! Only a one-line summary of each method's documentations is provided, please
 //! refer to the corresponding trait method's documentation in `num_traits` for
-//! the full gory details.
+//! the full details of each function's API contract.
+//!
+//! One advantage of trait methods over free functions is that they gracefully
+//! handle namespace collisions. Whereas with this crate, you will be the one
+//! responsible for `use`-ing the right methods for a given task. For what it's
+//! worth, this is why programming languages with prefix numerical methods
+//! usually also support method overloading, which Rust could not do as it
+//! breaks the kind of advanced type inference that we're all used to.
+//!
+//! # What's included
+//!
+//! This crate generally opts for maximal coverage of the num traits, except in
+//! the following circumstances:
+//!
+//! - The trait represents an operator whose standard notation is closer to a
+//!   postfix method than to a prefix funciton, as is the case for most binary
+//!   operators. Thus, `CheckedAdd`, `CheckedDiv`, `CheckedMul`, `CheckedRem`,
+//!   `CheckedShl`, `CheckedShr`, `CheckedSub`, `Saturating`, `WrappingAdd`,
+//!   `WrappingMul`, `WrappingShl`, `WrappingShr`and `WrappingSub` are not
+//!   covered.
+//! - The num_trait crates already provides a set of free functions that cover
+//!   90% of a trait's functionality, and we re-export them. Thus, `One`,
+//!   `Signed` and `Zero` are not covered.
+//! - A specific trait method would require very significant support
+//!   infrastructure to be exposed by this crate, while its real-world
+//!   usefulness is debatable, either as a free function or in general. Thus
+//!   `FloatConst::TAU()` and `Num::from_str_radix` are not covered.
+//!
+//! If you find a num trait functionality which is neither exposed by this
+//! crate nor covered by the above list, this is likely an oversight from my
+//! part, please ping me about it.
+//!
+//! I am also willing to reconsider any point of the above policy if someone
+//! manages to make a good argument against it. Issues are welcome!
+
+// TODO: Discuss casting traits above
+    // NOTE: Casting traits from num_traits::cast are not exposed by this crate
+    //       because they would require very significant support infrastructure
+    //       and I'm not sure the need for them is as strong as that for free
+    //       float functions. Please ping me if you think otherwise.
+
+#![no_std]
+#![cfg(feature = "std")]
+extern crate std;
+
+// Only `no_std` builds actually use `libm`.
+#[cfg(all(not(feature = "std"), feature = "libm"))]
+extern crate libm;
 
 use core::num::FpCategory;
 
-// Re-export useful prefix functions from num_traits
+// Re-export useful free functions from num_traits
 pub use num_traits::{
     cast::cast,
     clamp, clamp_max, clamp_min,
@@ -59,6 +108,10 @@ macro_rules! define_prefix_methods {
 //
 // TODO: Try to nerd-snipe a macro expert like dtolnay into generalizing this,
 //       or proving that it cannot be done within macro_rules' limitations.
+//
+//       I don't want to go for full proc macros because the compile time hit is
+//       too high while declarative macros do the job, although in a clunky way.
+//
 macro_rules! define_method {
     ($trait_path:path : $method_name:ident() -> Self) => {
         pub fn $method_name<T: $trait_path>() -> T {
@@ -103,8 +156,19 @@ macro_rules! define_method {
     };
 }
 //
+// TODO: Add tests
 define_prefix_methods! {
+    /// Methods from the Bounded trait, exposed as free functions.
+    num_traits::bounds::Bounded => bounded {
+        /// Returns the smallest finite number this type can represent.
+        min_value() -> Self
+
+        /// Returns the largest finite number this type can represent.
+        max_value() -> Self
+    }
+
     /// Methods from the Float trait, exposed as free functions.
+    #[cfg(any(feature = "std", feature = "libm"))]
     num_traits::float::Float => float {
         /// Returns the `NaN` value.
         nan() -> Self
@@ -130,7 +194,7 @@ define_prefix_methods! {
         /// Returns `true` if this value is `NaN` and false otherwise.
         is_nan(self) -> bool
 
-        /// Returns `true` if this value is positive infinity or negative infinity
+        /// Returns `true` if this value is positive or negative infinity
         /// and `false` otherwise.
         is_infinite(self) -> bool
 
@@ -150,39 +214,40 @@ define_prefix_methods! {
         ceil(self) -> Self
 
         /// Returns the nearest integer to a number.
+        /// Rounds half-way cases away from `0.0`.
         round(self) -> Self
 
-        /// Return the integer part of a number.
+        /// Returns the integer part of a number.
         trunc(self) -> Self
 
         /// Returns the fractional part of a number.
         fract(self) -> Self
 
-        /// Computes the absolute value of self.
+        /// Computes the absolute value of a number.
         abs(self) -> Self
 
-        /// Returns a number that represents the sign of self.
+        /// Returns a number that represents the sign of the input.
         signum(self) -> Self
 
-        /// Returns true if self is positive.
+        /// Returns true if the input is positive.
         is_sign_positive(self) -> bool
 
-        /// Returns true if self is negative.
+        /// Returns true if the input is negative.
         is_sign_negative(self) -> bool
 
         /// Fused multiply-add.
         mul_add(self, a: Self, b: Self) -> Self
 
-        /// Take the reciprocal (inverse) of a number, `1/x`.
+        /// Takes the reciprocal (inverse) of a number, `1/x`.
         recip(self) -> Self
 
-        /// Raise a number to an integer power.
+        /// Raises a number to an integer power.
         powi(self, n: i32) -> Self
 
-        /// Raise a number to a real number power.
+        /// Raises a number to a floating point power.
         powf(self, n: Self) -> Self
 
-        /// Take the square root of a number.
+        /// Takes the square root of a number.
         sqrt(self) -> Self
 
         /// Returns `e^x`, (the exponential function).
@@ -212,10 +277,11 @@ define_prefix_methods! {
         /// The positive difference of two numbers.
         abs_sub(self, other: Self) -> Self
 
-        /// Take the cubic root of a number.
+        /// Takes the cubic root of a number.
         cbrt(self) -> Self
 
-        /// Calculate the length of the hypotenuse of a right-angle triangle given leg lengths.
+        /// Computes the length of the hypotenuse of a right-angle triangle
+        /// given its legs' lengths.
         hypot(self, other: Self) -> Self
 
         /// Computes the sine of a number (in radians).
@@ -236,16 +302,19 @@ define_prefix_methods! {
         /// Computes the arctangent of a number.
         atan(self) -> Self
 
-        /// Computes the four quadrant arctangent of self (y) and other (x).
+        /// Computes the four quadrant arctangent of two numbers.
         atan2(self, other: Self) -> Self
 
-        /// Simultaneously computes the sine and cosine of a number.
+        /// Simultaneously computes the sine and cosine of a number `x`.
+        /// Returns `(sin(x), cos(x))`.
         sin_cos(self) -> (Self, Self)
 
-        /// Returns `e^(self) - 1` in a way that is accurate even if the number is close to zero.
+        /// Returns `e^(self) - 1` in a way that is accurate even
+        /// if the number is close to zero.
         exp_m1(self) -> Self
 
-        /// Returns `ln(1+n)` (natural logarithm) more accurately than if the operations were performed separately.
+        /// Returns `ln(1+n)` (natural logarithm) more accurately than
+        /// if the operations were performed separately.
         ln_1p(self) -> Self
 
         /// Hyperbolic sine function.
@@ -282,57 +351,53 @@ define_prefix_methods! {
     /// Methods from the FloatConst trait, exposed as free functions.
     #[allow(non_snake_case)]
     num_traits::float::FloatConst => float_const {
-        /// Return Euler’s number.
+        /// Returns Euler’s number.
         E() -> Self
 
-        /// Return `1.0 / π`.
+        /// Returns `1.0 / π`.
         FRAC_1_PI() -> Self
 
-        /// Return `1.0 / sqrt(2.0)`.
+        /// Returns `1.0 / sqrt(2.0)`.
         FRAC_1_SQRT_2() -> Self
 
-        /// Return `2.0 / π`.
+        /// Returns `2.0 / π`.
         FRAC_2_PI() -> Self
 
-        /// Return `2.0 / sqrt(π)`.
+        /// Returns `2.0 / sqrt(π)`.
         FRAC_2_SQRT_PI() -> Self
 
-        /// Return `π / 2.0`.
+        /// Returns `π / 2.0`.
         FRAC_PI_2() -> Self
 
-        /// Return `π / 3.0`.
+        /// Returns `π / 3.0`.
         FRAC_PI_3() -> Self
 
-        /// Return `π / 4.0`.
+        /// Returns `π / 4.0`.
         FRAC_PI_4() -> Self
 
-        /// Return `π / 6.0`.
+        /// Returns `π / 6.0`.
         FRAC_PI_6() -> Self
 
-        /// Return `π / 8.0`.
+        /// Returns `π / 8.0`.
         FRAC_PI_8() -> Self
 
-        /// Return `ln(10.0)`.
+        /// Returns `ln(10.0)`.
         LN_10() -> Self
 
-        /// Return `ln(2.0)`.
+        /// Returns `ln(2.0)`.
         LN_2() -> Self
 
-        /// Return `log10(e)`.
+        /// Returns `log10(e)`.
         LOG10_E() -> Self
 
-        /// Return `log2(e)`.
+        /// Returns `log2(e)`.
         LOG2_E() -> Self
 
-        /// Return Archimedes’ constant `π`.
+        /// Returns Archimedes’ constant `π`.
         PI() -> Self
 
-        /// Return `sqrt(2.0)`.
+        /// Returns `sqrt(2.0)`.
         SQRT_2() -> Self
-
-        // NOTE: Not supporting TAU(), as it requires adding support for trait
-        //       bounds in the above macro, which is gonna be pretty gnarly, and
-        //       it is debatable that its usefulness is worth it.
     }
 
     /// Methods from the FloatCore trait, exposed as free functions.
@@ -343,7 +408,7 @@ define_prefix_methods! {
         /// Returns negative infinity.
         neg_infinity() -> Self
 
-        /// Returns NaN.
+        /// Returns `NaN`.
         nan() -> Self
 
         /// Returns `-0.0`.
@@ -373,16 +438,16 @@ define_prefix_methods! {
         /// Returns the mantissa, base 2 exponent, and sign as integers, respectively.
         integer_decode(self) -> (u64, i16, i8)
 
-        /// Returns true if the number is NaN.
+        /// Returns true if the number is `NaN`.
         is_nan(self) -> bool
 
         /// Returns true if the number is infinite.
         is_infinite(self) -> bool
 
-        /// Returns true if the number is neither infinite or NaN.
+        /// Returns true if the number is neither infinite or `NaN`.
         is_finite(self) -> bool
 
-        /// Returns true if the number is neither zero, infinite, subnormal or NaN.
+        /// Returns true if the number is neither zero, infinite, subnormal or `NaN`.
         is_normal(self) -> bool
 
         /// Returns the largest integer less than or equal to a number.
@@ -391,25 +456,25 @@ define_prefix_methods! {
         /// Returns the smallest integer greater than or equal to a number.
         ceil(self) -> Self
 
-        /// Returns the nearest integer to a number. Round half-way cases away from 0.0.
+        /// Returns the nearest integer to a number. Round half-way cases away from `0.0`.
         round(self) -> Self
 
-        /// Return the integer part of a number.
+        /// Returns the integer part of a number.
         trunc(self) -> Self
 
         /// Returns the fractional part of a number.
         fract(self) -> Self
 
-        /// Computes the absolute value of self.
+        /// Computes the absolute value of a number.
         abs(self) -> Self
 
-        /// Returns a number that represents the sign of self.
+        /// Returns a number that represents the sign of.
         signum(self) -> Self
 
-        /// Returns true if self is positive.
+        /// Returns true if the input is positive.
         is_sign_positive(self) -> bool
 
-        /// Returns true if self is negative.
+        /// Returns true if the input is negative.
         is_sign_negative(self) -> bool
 
         /// Returns the minimum of the two numbers.
@@ -427,16 +492,16 @@ define_prefix_methods! {
 
     /// Methods from the PrimInt trait, exposed as free functions.
     num_traits::int::PrimInt => prim_int {
-        /// Returns the number of ones in the binary representation of `self`.
+        /// Returns the number of ones in the binary representation of the input.
         count_ones(self) -> u32
 
-        /// Returns the number of zeros in the binary representation of `self`.
+        /// Returns the number of zeros in the binary representation of the input.
         count_zeros(self) -> u32
 
-        /// Returns the number of leading zeros in the binary representation of `self`.
+        /// Returns the number of leading zeros in the binary representation of the input.
         leading_zeros(self) -> u32
 
-        /// Returns the number of trailing zeros in the binary representation of `self`.
+        /// Returns the number of trailing zeros in the binary representation of the input.
         trailing_zeros(self) -> u32
 
         /// Shifts the bits to the left by a specified amount amount, `n`,
@@ -472,17 +537,18 @@ define_prefix_methods! {
         /// Convert an integer from little endian to the target's endianness.
         from_le(x: Self) -> Self
 
-        /// Convert self to big endian from the target's endianness.
+        /// Convert the input to big endian from the target's endianness.
         to_be(self) -> Self
 
-        /// Convert self to little endian from the target's endianness.
+        /// Convert the input to little endian from the target's endianness.
         to_le(self) -> Self
 
-        /// Raises self to the power of exp, using exponentiation by squaring.
+        /// Raises a number to the power of exp, using exponentiation by squaring.
         pow(self, exp: u32) -> Self
     }
 
     /// Methods from the Real trait, exposed as free functions.
+    #[cfg(any(feature = "std", feature = "libm"))]
     num_traits::real::Real => real {
         /// Returns the smallest finite value that this type can represent.
         min_value() -> Self
@@ -505,22 +571,22 @@ define_prefix_methods! {
         /// Returns the nearest integer to a number.
         round(self) -> Self
 
-        /// Return the integer part of a number.
+        /// Returns the integer part of a number.
         trunc(self) -> Self
 
         /// Returns the fractional part of a number.
         fract(self) -> Self
 
-        /// Computes the absolute value of self.
+        /// Computes the absolute value of a number.
         abs(self) -> Self
 
-        /// Returns a number that represents the sign of self.
+        /// Returns a number that represents the sign of the input.
         signum(self) -> Self
 
-        /// Returns true if self is positive.
+        /// Returns true if the input is positive.
         is_sign_positive(self) -> bool
 
-        /// Returns true if self is negative.
+        /// Returns true if the input is negative.
         is_sign_negative(self) -> bool
 
         /// Fused multiply-add.
@@ -562,19 +628,20 @@ define_prefix_methods! {
         /// Converts degrees to radians.
         to_radians(self) -> Self
 
-        /// Returns the maximum of the two numbers.
+        /// Returns the maximum of two numbers.
         max(self, other: Self) -> Self
 
-        /// Returns the minimum of the two numbers.
+        /// Returns the minimum of two numbers.
         min(self, other: Self) -> Self
 
-        /// The positive difference of two numbers.
+        /// Returns the positive difference of two numbers.
         abs_sub(self, other: Self) -> Self
 
-        /// Take the cubic root of a number.
+        /// Takes the cubic root of a number.
         cbrt(self) -> Self
 
-        /// Calculate the length of the hypotenuse of a right-angle triangle given leg lengths.
+        /// Computes the length of the hypotenuse of a right-angle triangle
+        /// given its legs' lengths.
         hypot(self, other: Self) -> Self
 
         /// Computes the sine of a number (in radians).
@@ -595,16 +662,19 @@ define_prefix_methods! {
         /// Computes the arctangent of a number.
         atan(self) -> Self
 
-        /// Computes the four quadrant arctangent of self (y) and other (x).
+        /// Computes the four quadrant arctangent of two numbers.
         atan2(self, other: Self) -> Self
 
-        /// Simultaneously computes the sine and cosine of a number.
+        /// Simultaneously computes the sine and cosine of a number `x`.
+        /// Returns `(sin(x), cos(x))`.
         sin_cos(self) -> (Self, Self)
 
-        /// Returns `e^(self) - 1` in a way that is accurate even if the number is close to zero.
+        /// Returns `e^(self) - 1` in a way that is accurate even if
+        /// the number is close to zero.
         exp_m1(self) -> Self
 
-        /// Returns `ln(1+n)` (natural logarithm) more accurately than if the operations were performed separately.
+        /// Returns `ln(1+n)` (natural logarithm) more accurately than
+        /// if the operations were performed separately.
         ln_1p(self) -> Self
 
         /// Hyperbolic sine function.
@@ -626,5 +696,5 @@ define_prefix_methods! {
         atanh(self) -> Self
     }
 
-    // TODO: Do the same thing for other traits
+    // TODO: Add CheckedNeg, Inv, MulAdd and MulAddAssign
 }
